7c417eecf580dfae54c03d7fc1ff87e295d8c763
SleepingNerd <samiarre08@gmail.com>
2023-11-01 21:27:53 +0100

    Step based collision done, I think I could optimise it by moving all in one movement, and then just checking collision from closest to farthest but oh well

--- src/player.c
+++ src/player.c
@@ -36,29 +36,11 @@ void ProcessInput(Player *player, bool left, bool right, bool jump)
     } 
 }
 
-// Applies and updates velocity and position, applies gravity
-void MoveAndUpdate(Player *player, float delta, World world)
+// Assumes player last moved down and applies collision accordingly, should be used as sparingly as possible,
+// Always check if player actually crossed a tile before calling
+void DownCollision(Player *player, World world, int left_tile, int right_tile)
 {
-    int left_tile = (player->rect.x)/TILE_SIZE;
-    int right_tile = (player->rect.x+player->p_offset.x)/TILE_SIZE;
-
-    int top_tile;
-    int bottom_tile;
-    
-
-    float to_be_moved_y = player->velocity.y*delta;
-    player->rect.y += to_be_moved_y;
-
-
-    
-
-    
-   
-
-    // Going down
-    if (player->velocity.y> 0)
-    {
-        bottom_tile = (player->rect.y+player->p_offset.y)/TILE_SIZE;
+    int bottom_tile = (player->rect.y+player->p_offset.y)/TILE_SIZE;
 
 
        
@@ -72,6 +54,96 @@ void MoveAndUpdate(Player *player, float delta, World world)
             }
 
         }
+}
+
+// Assumes player last moved up and applies collision accordingly, should be used as sparingly as possible,
+// Always check if player actually crossed a tile before calling
+void UpCollision(Player *player, World world, int left_tile, int right_tile)
+{
+    int top_tile = (player->rect.y)/TILE_SIZE;
+
+    for(int i = left_tile; i<=right_tile; i++)
+    {
+        // Apply setback if moved into tile
+        if (world.arr[(i)+(top_tile *world.size.x)].type == SAND)
+        {
+            player->rect.y = (top_tile+1)*TILE_SIZE;
+            break;
+        }
+
+    }
+}
+
+// Assumes player last moved right and applies collision accordingly, should be used as sparingly as possible,
+// Always check if player actually crossed a tile before calling
+void RightCollision(Player *player, World world, int top_tile, int bottom_tile)
+{
+    int right_tile = (player->rect.x+player->p_offset.x)/TILE_SIZE;
+
+        for(int i = top_tile; i<= bottom_tile; i++)
+        {
+            if (world.arr[(right_tile)+(i *world.size.x)].type == SAND)
+            {
+                player->rect.x = right_tile*TILE_SIZE-player->rect.width;
+            }
+        }
+}
+
+void LeftCollision(Player *player, World world, int top_tile, int bottom_tile)
+{
+    int left_tile = (player->rect.x)/TILE_SIZE;
+    for(int i = top_tile; i<= bottom_tile; i++)
+    {
+        if (world.arr[(left_tile)+(i *world.size.x)].type == SAND)
+        {
+            player->rect.x = (left_tile+1)*TILE_SIZE;
+        }
+    }
+}
+
+
+
+// Applies and updates velocity and position, applies gravity
+void MoveAndUpdate(Player *player, float delta, World world)
+{
+    int left_tile = (player->rect.x)/TILE_SIZE;
+    int right_tile = (player->rect.x+player->p_offset.x)/TILE_SIZE;
+
+    int top_tile;
+    int bottom_tile;
+    
+    
+    Vector2 to_be_moved = {.y = player->velocity.y*delta, .x =player->velocity.x*delta};
+    Vector2 offset_from_tile = {.x= fmodf(player->rect.x, TILE_SIZE), .y= fmodf(player->rect.y, TILE_SIZE)};
+    Vector2 move_offset = {.x =  fmodf(player->velocity.x*delta, TILE_SIZE), .y= fmodf(player->velocity.y*delta, TILE_SIZE)};
+
+   
+
+   
+
+    // Going down
+    if (player->velocity.y> 0)
+    {
+
+        for (int i = 0; i<(int)to_be_moved.y; i+=TILE_SIZE)
+        {
+            player->rect.y += TILE_SIZE;
+            DownCollision(player, world, left_tile, right_tile);
+
+        }
+        
+
+        player->rect.y += move_offset.y;
+
+        
+        // Collide again
+        if ((offset_from_tile.y+move_offset.y) >= 1)
+        {
+            
+            DownCollision(player, world, left_tile, right_tile);
+        }
+
+
 
         // Recalculate top and bottom after movement
         top_tile = (player->rect.y)/TILE_SIZE;
@@ -80,18 +152,28 @@ void MoveAndUpdate(Player *player, float delta, World world)
 
     else if (player->velocity.y< 0)
     {
-        top_tile = (player->rect.y)/TILE_SIZE;
-
-        for(int i = left_tile; i<=right_tile; i++)
+        
+        for (int i = 0; i>(int)to_be_moved.y; i-=TILE_SIZE)
         {
-            // Apply setback if moved into tile
-            if (world.arr[(i)+(top_tile *world.size.x)].type == SAND)
-            {
-                player->rect.y = (top_tile+1)*TILE_SIZE;
-                break;
-            }
+            player->rect.y -= TILE_SIZE;
+            UpCollision(player, world, left_tile, right_tile);
 
         }
+        
+        player->rect.y += move_offset.y;
+
+        // Collide again
+        if ((offset_from_tile.y+move_offset.y) <= 0)
+        {
+            
+            UpCollision(player, world, left_tile, right_tile);
+        }
+
+
+
+
+
+
         // Recalculate top and bottom after movement
         top_tile = (player->rect.y)/TILE_SIZE;
         bottom_tile = (player->rect.y+player->p_offset.y)/TILE_SIZE;
@@ -103,40 +185,47 @@ void MoveAndUpdate(Player *player, float delta, World world)
 
     
     
-    player->rect.x += player->velocity.x*delta;
 
     left_tile = (player->rect.x)/TILE_SIZE;
     right_tile = (player->rect.x+player->p_offset.x)/TILE_SIZE;
+
+
 
     
 
     // Going right
     if (player->velocity.x > 0)
     {
-
-        right_tile = (player->rect.x+player->p_offset.x)/TILE_SIZE;
-
-        for(int i = top_tile; i<= bottom_tile; i++)
+        for (int i = 0; i<(int)to_be_moved.x; i+=TILE_SIZE)
         {
-            if (world.arr[(right_tile)+(i *world.size.x)].type == SAND)
-            {
-                player->rect.x = right_tile*TILE_SIZE-player->rect.width;
-
-            }
+            player->rect.x += 1;
+            RightCollision(player, world, top_tile, bottom_tile);
         }
+
+        player->rect.x += move_offset.x;
+        if ((offset_from_tile.x+move_offset.x)>=1)
+        {
+            RightCollision(player, world, top_tile, bottom_tile);
+        }
+
         player->facing = RIGHT;
 
     }
 
     else if (player->velocity.x <0)
     {
-        for(int i = top_tile; i<= bottom_tile; i++)
+        for (int i = 0; i>(int)to_be_moved.x; i-=TILE_SIZE)
         {
-            if (world.arr[(left_tile)+(i *world.size.x)].type == SAND)
-            {
-                player->rect.x = (left_tile+1)*TILE_SIZE;
-            }
+            player->rect.x -= 1;
+            LeftCollision(player, world, top_tile, bottom_tile);
         }
+
+        player->rect.x += move_offset.x;
+        if ((offset_from_tile.x+move_offset.x)<=0)
+        {
+            LeftCollision(player, world, top_tile, bottom_tile);
+        }
+        
         player->facing = LEFT;
     }
 
